162454b9d8992954462416c084e525b89ab7f88a
Michael Phelps <michaeljphelps1978@gmail.com>
2020-11-08 20:59:43 -0600

    :sparkles: Exercise 4 - Stretch
    Complete - Tests pass

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,24 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return result
+   ht = {}
+   result = []
+
+   # loop through the list, and set a desired value of the current number's negative value (by multiplying by -1)
+
+   for number in a:
+       desired = number * (-1)
+
+   ## if the desired number is less than the current number (the negative opposite) - Then it has a negative - Add it to the results
+   ## otherwise, we're adding the desired number
+       if desired in ht:
+           if number > desired:
+               result.append(number)
+           else:
+               result.append(desired)
+   ## if it's not in the table, then we need to add it, by setting a key of the number, and a value of it's opposite
+       else:
+           ht[number] = desired
+   return result
 
 
 if __name__ == "__main__":
