e0c26795807bcc4700dafa6730bbb2b93ea16e71
Michael Phelps <michaeljphelps1978@gmail.com>
2020-11-08 20:16:20 -0600

    :sparkles: Exercise 1
    Completed and passed tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,13 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return None
+   ht = {}
+   # as we cycle through the weights, we want to calculate the desired weight and then check it against the hashtable
+   for i, w in enumerate(weights):
+       desired_weight = limit - w
+
+       if desired_weight in ht:
+           return(i, ht[desired_weight])
+       # and if it's not there, we need to add it
+       ht[w] = i
+
+   return None
