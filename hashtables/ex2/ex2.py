4432fad7dc69f234f430842f129024af774ef079
Michael Phelps <michaeljphelps1978@gmail.com>
2020-11-08 20:32:50 -0600

    :sparkles: Exercise 2
    Complete and Tests passing.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,24 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
+    # Setting up a data structure, that contains a hashtable of tickets, and a list that will contain our final route
+    ticket_collection = {}
+    route = []
+
+    # populate our hashtable with all of the tickets - key = origin, value = destination
+    for ticket in tickets:
+        ticket_collection[ticket.source] = ticket.destination
+
+    # start off with our "home" location (looking at the key)
+    cur = ticket_collection['NONE']
+
+    # now we run through the tickets, looking at the key, and
+    # begin populating our route with the data we need (linking the value with the next key)
+    while cur != 'NONE':
+        route.append(cur)
+        cur = ticket_collection[cur]
+
+    route.append('NONE')
     return route
+
